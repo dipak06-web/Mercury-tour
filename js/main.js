d373aeaab0517321d1ba6ba2db31b1a31d906ea1
mac <mac@macs-MacBook-Pro-6.local>
2024-06-21 22:51:11 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$('#banner .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    animateOut: 'fadeOut',
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+
+
+$('.fff .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    animateOut: 'fadeOut',
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+
+$('.pictures .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    // autoplay: true,
+    // autoplayTimeout: 2000,
+    animateOut: 'fadeOut',
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
